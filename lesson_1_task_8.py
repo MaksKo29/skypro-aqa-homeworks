04f542cdc27684c48c2562b88d7e91435a276c4a
MaksKo29 <maksko29@gmail.com>
2024-01-05 16:10:06 +0300

    New code

--- lesson_1_task_8.py
+++ lesson_1_task_8.py
@@ -1,15 +1,9 @@
-num1 = input('Number:')
-num2 = input('Number:')
-num3 = input('Number:')
-num4 = input('Number:')
-num5 = input('Number:')
-num6 = input('Number:')
-num7 = input('Number:')
-num8 = input('Number:')
-num9 = input('Number:')
-num10 = input('Number:')
-num11 = input('Number:')
+results = []   
+def my_function():
+    return input()
+ 
+for _ in range(11):
+    result = my_function()
+    results.append(result)
 
-print(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, sep='')
-
-
+print(*results, sep='') 
